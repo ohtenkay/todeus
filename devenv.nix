d60b40c7ff3cb9dafd5affda0e7a972fc41e2a8b
Ondrej Hlozek <hlozek.o1@gmail.com>
2026-06-20 19:45:45 +0200

    feat: emulator command

--- devenv.nix
+++ devenv.nix
@@ -8,6 +8,11 @@
 
   languages.dart.enable = true;
 
+  scripts.start-emulator.exec = ''
+    export ANDROID_AVD_HOME="$PWD/.android/avd"
+    exec emulator -avd pixel-6-pro-api-36 -gpu host
+  '';
+
   # Work around https://github.com/cachix/devenv/issues/2782: devenv's Android
   # shell puts build-tools lib64 on LD_LIBRARY_PATH, which makes the emulator
   # load the wrong libc++.so and fail with a libabseil_dll.so symbol error.
